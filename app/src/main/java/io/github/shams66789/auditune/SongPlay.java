b1e9ef72e93da711090616b8c63695e3101466b3
Shams66789 <rajansari66789@gmail.com>
2022-03-17 12:50:20 +0530

    Layout for playing the music made

---
+++ app/src/main/java/io/github/shams66789/auditune/SongPlay.java
@@ -0,0 +1,14 @@
+package io.github.shams66789.auditune;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SongPlay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_song_play);
+    }
+}
